be971e29d107615d6bf67475c55b3a00af0aa665
Fabrizzio Rivera <frivera@nevicad.com>
2019-09-30 13:34:33 -0600

    partial cli

--- index.js
+++ index.js
@@ -1,8 +1,67 @@
 import { Lexer, Parser } from "./src/Compiler";
 import * as fs from 'fs'
+import * as inquirer from 'inquirer'
+const inquirerFileTreeSelection = require('inquirer-file-tree-selection-prompt')
 
-const path = `${process.cwd()}\\${process.argv[2]}`
-const code = fs.readFileSync(path,'utf8')
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection)
+
+//const path = `${process.cwd()}\\${process.argv[2]}`
+//const code = fs.readFileSync(path,'utf8')
+console.log(process.argv)
+
+inquirer.prompt(
+  [
+    {
+      type: 'file-tree-selection',
+      name: 'input',
+      message: 'Select input file'
+    },
+    {
+      type: 'input',
+      name: 'output',
+      message: "Select output name"
+    },
+    {
+      type: 'list',
+      name: 'stage',
+      message: 'Select stage',
+      choices: [
+        'Scanning',
+        'Parsing',
+        {
+          name: 'AST',
+          disabled: 'Unavailable at this time'
+        },
+        {
+          name: 'Semantic',
+          disabled: 'Unavailable at this time'
+        },
+        {
+          name: 'IRT',
+          disabled: 'Unavailable at this time'
+        },
+        {
+          name: 'Codegen',
+          disabled: 'Unavailable at this time'
+        }
+      ]
+    },
+    {
+      type: 'list',
+      name: 'opt',
+      message: 'Select optimization',
+      choices: [
+        'Constant',
+        'Algebraic',
+      ]
+    }
+  ])
+  .then(answers => {
+    console.log(answers);
+  }
+);
+
+/*
 
 const rules = JSON.parse(fs.readFileSync('./regex/tokensRegex.json', 'utf8'))
 const grammar = JSON.parse(fs.readFileSync('./regex/grammar.json', 'utf8'))
@@ -14,3 +73,4 @@ const parse = new Parser(grammar, tokens)
 console.log(tokens)
 console.log('---')
 console.log(JSON.stringify(parse.parse(), null, 2))
+*/
