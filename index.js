920feba79dd28f106701f57297f2e6537fbaee18
Fabrizzio Rivera <frivera@nevicad.com>
2019-09-30 11:17:04 -0600

    directory change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { Lexer, Parser } from "./Compiler";
+import { Lexer, Parser } from "./src/Compiler";
 import * as fs from 'fs'
 
 const path = `${process.cwd()}\\${process.argv[2]}`
 const code = fs.readFileSync(path,'utf8')
 
-const rules = JSON.parse(fs.readFileSync('tokensRegex.json', 'utf8'))
-const grammar = JSON.parse(fs.readFileSync('grammar.json', 'utf8'))
+const rules = JSON.parse(fs.readFileSync('../regex/tokensRegex.json', 'utf8'))
+const grammar = JSON.parse(fs.readFileSync('../regex/grammar.json', 'utf8'))
 
 const Lex = new Lexer(rules, code)
 const tokens = Lex.tokenize()
